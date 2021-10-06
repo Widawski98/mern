432a5bf04332eb7953536177a77692e243fe53b5
Widawski98 <widawskidev@gmail.com>
2021-10-06 14:46:31 +0200

    github share code

---
+++ shop/frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Acer Nitro 5',
+            category:'PC',
+            image:'./images/product1.jpg',
+            price:1000,
+            brand:'Acer',
+            rating:4.5,
+            numRevievs:10,
+            description:'Acer pc game',
+
+        },
+             {
+            _id:'2',
+            name:'Acer Nitro 6',
+            category:'PC',
+            image:'./images/product2.jpg',
+            price:3230,
+            brand:'Acer',
+            rating:2.5,
+            numRevievs:10,
+            description:'Acer pc game',
+
+        },
+             {
+            _id:'3',
+            name:'Acer Nitro 7',
+            category:'PC',
+            image:'./images/product3.jpg',
+            price:325600,
+            brand:'Acer',
+            rating:1.5,
+            numRevievs:10,
+            description:'Acer pc game',
+
+        },
+             {
+            _id:'4',
+            name:'Acer Nitro 8',
+            category:'PC',
+            image:'./images/product4.jpg',
+            price:3220,
+            brand:'Acer',
+            rating:3.5,
+            numRevievs:10,
+            description:'Acer pc game',
+
+        },
+             {
+            _id:'5',
+            name:'Acer Nitro 9',
+            category:'PC',
+            image:'./images/product5.jpg',
+            price:3220,
+            brand:'Acer',
+            rating:4.5,
+            numRevievs:1,
+            description:'Acer pc game',
+
+        },
+             {
+            _id:'6',
+            name:'Acer Nitro 10',
+            category:'PC',
+            image:'./images/product6.jpg',
+            price:3600,
+            brand:'Acer',
+            rating:5,
+            numRevievs:20,
+            description:'Acer pc game',
+
+        },
+    ]
+}
+export default data;
